b7a7af6c9c40dbbc47f51f494aeaf8f8b00b95c6
Williams Alves Dantas <williamsad9@gmail.com>
2024-09-23 16:20:42 -0300

    correção de bugs no seviço de pedidos

--- venv/routes/pedidos.py
+++ venv/routes/pedidos.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from config.config import pedidos_collection,clientes_collection, produtos_collection
 from models.pedido import Pedidos
-from services.pedidos_service import lista_pedidos, get_pedido_by_id, create_pedido
+from services.pedidos_service import lista_todos_pedidos, get_pedido_by_id, create_pedido, delete_pedido, update_pedido
 
 
 pedidos_bp = Blueprint('pedidos_bp', __name__)
@@ -9,18 +9,54 @@ pedidos_bp = Blueprint('pedidos_bp', __name__)
 @pedidos_bp.route("/pedidos",methods=['GET'])
 def lista_pedidos():
     try:
-        pedidos = lista_pedidos() 
+        pedidos = lista_todos_pedidos() 
         
-        for pedido in pedidos:
-            pedido['_id'] = str(pedido['_id'])
-        
-        return jsonify(pedido), 200
-
+        if pedidos:
+            for pedido in pedidos:
+                pedido['_id'] = str(pedido['_id'])
+            return jsonify(pedidos), 200
+        else: return None
     except Exception as e:
         print(f"Erro: {e}")
         return "Erro ao listar pedidos.", 500
     
+@pedidos_bp.route("/pedidos/<id_pedido>", methods=["GET"])
+def consultaPedido_por_id(id_pedido):
+    try:
+         # Converter id_pedido para inteiro 
+        id_pedido = int(id_pedido)
+        
+        pedido = get_pedido_by_id(id_pedido)
+        
+        if pedido:
+            pedido['_id'] = str(pedido['_id'])
+            return jsonify(pedido), 200 
+        else:           
+            return f"O id {id_pedido} informado não foi encontrado.",200
+    except ValueError:
+        # Se o id_produto não for um número inteiro válido
+        return f"O ID precisa ser um número inteiro", 400
+    
+    
+@pedidos_bp.route("/alteraPedido/<id_pedido>", methods=["PUT"])
+def atualiza_pedido(id_pedido):
+    try:
+        dados = request.get_json()
 
+        # Converter id_pedido para inteiro (ajuste se necessário)
+        id_pedido = int(id_pedido)
+
+        # Atualiza o documento utilizando o _id
+        pedido_atualizacao = update_pedido(id_pedido, dados)
+    
+        if pedido_atualizacao:
+            return jsonify(pedido_atualizacao.serialize()),200
+        else:
+            return f"Erro ao atualizar pedido.", 500
+
+    except Exception as e:
+        return f"Erro ao atualizar pedido: {e}", 500
+    
 @pedidos_bp.route("/inserirPedido", methods=['POST'])
 def set_pedido():
     dados = request.get_json()
@@ -30,6 +66,25 @@ def set_pedido():
         return jsonify(pedido.serialize()), 201
     else:
         return pedido, 500  
+    
+@pedidos_bp.route("/excluiPedido/<id_pedido>", methods=["DELETE"])
+def excluir_pedido(id_pedido):
+    try:
+
+        # Converter id_pedido para inteiro (ajuste se necessário)
+        id_pedido = int(id_pedido)       
+        
+        resultado = delete_pedido(id_pedido)
+                    
+        if resultado:
+            return "", 204
+        else:
+            return f"pedido com id {id_pedido} não encontrado.", 404
+    except ValueError:
+        # Se o id_produto não for um número inteiro válido
+        return f"O ID precisa ser um número inteiro", 400
+    except Exception as e:
+        return {f"Erro ao excluir produto: {e}"}, 500
 
     
     
--- venv/services/pedidos_service.py
+++ venv/services/pedidos_service.py
@@ -7,11 +7,11 @@ from services.produto_service import get_produto_by_id
 
 # serviço responsável pela listagem  de pedidos. 
 # a implementação da consulta retorna uma lista ordenada pelo id_pedido
-def lista_pedidos():
+def lista_todos_pedidos():
     pedidos = pedidos_collection.find().sort("id_pedido",1)
     return [pedido for pedido in pedidos]
 
-# serviço responsável pela pesquisa de produtos pelo id_pedido
+# serviço responsável pela pesquisa de pedido pelo id_pedido
 # a implementação da pesquisa retorna um obj pedido , ou None.
 def get_pedido_by_id(id_pedido):
     try: 
@@ -24,18 +24,67 @@ def get_pedido_by_id(id_pedido):
     except Exception as e:
         print(f"Erro ao buscar pedido por ID: {e}")
         return None
+    
+# serviço responsável pela alteração de produto com o id informado.
+# a implementação altera o produto pelo id_produto, e retorna o objeto produto alterado.
+def update_pedido(id_pedido, dados):
+    try:
+    
+        pedido = get_pedido_by_id(id_pedido)
+        if pedido:
+            
+            id_cliente = dados['id_cliente']
+            id_produto = dados['id_produto']
+            
+            # Verificar se o cliente existe
+            cliente = get_cliente_by_id(id_cliente)     
+            if cliente is None:
+                return f"Cliente {id_cliente} não encontrado"
+
+            # Verificar se o produto existe
+            produto = get_produto_by_id(id_produto)         
+            if produto is None:
+                return f"Produto {id_produto} não encontrado"
+            
+            # Busca e atualiza o pedido usando find_one_and_update
+            pedido_atualizado = pedidos_collection.find_one_and_update(
+                {"id_pedido": int(id_pedido)},  # Condição de busca
+                {"$set": dados},  # Dados a serem atualizados
+                return_document=True  # Retorna o documento atualizado
+            )
+            
+            if pedido_atualizado:
+                # Retorna o pedido atualizado como um objeto pedido
+                return Pedidos(
+                    id_pedido=pedido_atualizado['id_pedido'], 
+                    id_cliente=pedido_atualizado['id_cliente'], 
+                    id_produto=pedido_atualizado['id_produto'], 
+                    valor=pedido_atualizado['valor'], 
+                    data_pedido=pedido_atualizado['data_pedido']
+                )
+            else:
+                # Se nenhum pedido foi atualizado
+                return None
+        else: return f"Não existe um pedido com o id {id_pedido}"
+
+    except Exception as e:
+        print(f"Erro ao atualizar pedido: {e}")
+        return None
 
 # serviço responsável pela criação de cliente no catálogo de clientes
 # a implementação não permite inclusão de clientes com id_cliente repetidos.
 def create_pedido(dados):    
     
-    id_cliente = dados['id_cliente']
-    id_produto = dados['id_produto']
+    
     id_pedido = dados['id_pedido']
     
     pedido = get_pedido_by_id(id_pedido)
     
     if pedido is None:
+        
+        id_cliente = dados['id_cliente']
+        id_produto = dados['id_produto']
+        
         # Verificar se o cliente existe
         cliente = get_cliente_by_id(id_cliente)     
         if cliente is None:
@@ -62,4 +111,28 @@ def create_pedido(dados):
             return None
     else:
         return f"Já existe um pedido com o id {id_pedido}"
+    
+# serviço responsável pela exclusão de pedido com o id informado.
+# a implementação pesquisa o pedido pelo id_pedido, mas exclui pelo "_id" do documento.
+#Retorna NOne, se o pedido não existir.
+def delete_pedido(id_pedido):
+    try:
+
+        # Converter id_pedido para inteiro (ajuste se necessário)
+        id_pedido = int(id_pedido)
+
+        # Buscar o documento utilizando o id_produto personalizado
+        resultado_busca = get_pedido_by_id(id_pedido)
+        
+        if resultado_busca:
+            _id = resultado_busca["_id"]
+
+            resultado = pedidos_collection.delete_one({"_id": _id})
+
+            if resultado.deleted_count == 1:
+                return True
+        return None
+
+    except Exception as e:
+        return f"Erro ao excluir o pedido: {e}", 500
     
