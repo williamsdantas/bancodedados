eb7154f50128cedab5d11a3d836f2b78821b8cde
Williams Alves Dantas <williamsad9@gmail.com>
2024-09-23 12:48:22 -0300

    refatoração da alteração dos produtos

--- venv/routes/produtos.py
+++ venv/routes/produtos.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from config.config import produtos_collection
 from models.produto import Produtos
-from services.produto_service import lista_todos_produtos, create_produto, get_produto_by_id, delete_produto
+from services.produto_service import lista_todos_produtos, create_produto, get_produto_by_id, delete_produto, update_produto
 
 produtos_bp = Blueprint('produtos_bp', __name__)
 
@@ -55,31 +55,20 @@ def set_produto():
 
 #
 @produtos_bp.route("/alteraProduto/<id_produto>", methods=["PUT"])
-def update_produto(id_produto):
+def atualiza_produto(id_produto):
     try:
         dados = request.get_json()
 
         # Converter id_produto para inteiro (ajuste se necessário)
         id_produto = int(id_produto)
 
-        # Buscar o documento utilizando o id_produto personalizado
-        resultado_busca = produtos_collection.find_one({"id_produto": id_produto})
-
-        if resultado_busca:
-            _id = resultado_busca["_id"]
-
-            # Atualiza o documento utilizando o _id
-            resultado_atualizacao = produtos_collection.update_one(
-                {"_id": _id},
-                {"$set": dados}
-            )
-
-            if resultado_atualizacao.modified_count == 1:
-                return f"produto {id_produto} atualizado com sucesso.", 200
-            else:
-                return f"Erro ao atualizar produto.", 500
+        # Atualiza o documento utilizando o _id
+        produto_atualizacao = update_produto(id_produto, dados)
+    
+        if produto_atualizacao:
+            return jsonify(produto_atualizacao.serialize()),200
         else:
-            return f"produto com id {id_produto} não encontrado.", 404
+            return f"Erro ao atualizar produto.", 500
 
     except Exception as e:
         return f"Erro ao atualizar produto: {e}", 500
